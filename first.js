bcb4c8b8f94b6cb136f11bc22fae1ba71adc744f
Vidhya Murthy <vidhyamurthy@Vinods-MacBook-Pro.local>
2018-09-13 19:10:37 +0530

    Some simple java script code

---
+++ first.js
@@ -0,0 +1,21 @@
+//demonstration of single line comment
+var firstName = 'Tanya';
+console.log(firstName + ' Hello World');
+alert(firstName + ' Hello World');
+
+//simple operator 
+var year = 2018;
+var age = 98;
+var birthYear = year - age;
+
+console.log(birthYear);
+
+
+/**
+ * multi line 
+ * comment
+ */
+
+ var x = y = 5 + 2 /10;
+ console.log(x);
+ console.log(y);
