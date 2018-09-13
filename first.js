4415968352e71c2ebe488843703e51a54539e0d1
vidhya.nsmurthy@gmail.com <vidhyamurthy@Vinods-MacBook-Pro.local>
2018-09-13 21:56:35 +0530

    Some more code snippets

--- first.js
+++ first.js
@@ -16,6 +16,59 @@ console.log(birthYear);
  * comment
  */
 
+ /*code to show assignments go from right to left while 
+ statements get evaluated from left to right. also demonstrates
+ operator precedence
+*/
  var x = y = 5 + 2 /10;
  console.log(x);
  console.log(y);
+
+ /* strict equality check vs equality check with type coercion
+ also demonstrates if else if else block
+ */
+ var age1 = 23; //a number
+ var age2 = '23'; //a string
+
+ if (age1 === age2) {
+     console.log('this should not be logged');
+ } else if (age1 == age2) {
+     console.log('this should be logged');
+ } else {
+     console.log('this again should not be logged');
+ }
+
+//demonstration of the switch case construct
+var occupation = 'chef';
+switch(occupation) {
+    case 'chef': 
+      console.log(firstName + ' is a chef');
+      break;
+    case 'teacher':
+      console.log(firstName + ' is a teacher');
+      break;
+    default: 
+      console.log (firstName + ' is a musician');   
+}
+
+
+//truthy and falsy statements
+var myVar;
+
+if (myVar) {
+    console.log('myVar is undefined hence this should not be printed');
+} 
+
+myVar = null;
+
+if (!myVar) {
+    console.log('null is falsy so this should be printed');
+} 
+
+
+
+
+
+
+
+
